c528498d37e5017b38524330ffbafdb49e479e28
Jha Kanhaiya <79010342+mekanhaji@users.noreply.github.com>
2023-09-28 14:38:35 +0000

    INITIATED

---
+++ Routes/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'This is secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
